73874b7f8eb4b95933cb72e63d7111210115e693
Musasizi Francis <musasizifrancis@gmail.com>
2018-10-06 14:43:57 +0300

    my first todo app

---
+++ Todo.py
@@ -0,0 +1,18 @@
+class todo():
+    def __init__(self, created_at, name ):
+         self.created_at = created_at
+         self.name = name
+
+def add_tasks(craeted_at, name):
+    self.name = name
+    return.self = name
+
+    def show_tasks(created_at, name):
+        self.name = name
+        return.self = name
+
+    def delete_tasks(created_at, name):
+        self.name = name
+        return.self = name
+
+
